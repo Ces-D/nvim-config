3566cd1284ed0fec5ef1c294317feac910bb6fad
Ces-D <cesdia883@gmail.com>
2022-10-12 11:20:53 -0400

    fzf extension

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -139,7 +139,9 @@ local plugins = {
 
   { "BurntSushi/ripgrep" },
 
-  { "nvim-telescope/telescope-fzf-native.nvim" }
+  { "nvim-telescope/telescope-fzf-native.nvim",
+    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  }
 }
 
 -- load the plugins
