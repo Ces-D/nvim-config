6dfb7a5e589aca51848cbf886c01b653b252fc4a
Ces-D <cesdia883@gmail.com>
2023-03-28 09:39:35 -0400

    fix init script error

--- lua/cesd/init.lua
+++ lua/cesd/init.lua
@@ -1,17 +1,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- latest stable release lazypath,
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable" })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("cesd.options")
 require("lazy").setup("cesd.plugins")
 local utils = require("cesd.utils")
-
-for _, setup_plugin in pairs(require("cesd.setups")) do
-  setup_plugin()
-end
 
 for _, mapping in pairs(require("cesd.mappings")) do
   utils.load_mappings(mapping)
