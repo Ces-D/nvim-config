f23c5eb3f00965502bccd9672918815d7748f82f
Ces-D <cesdia883@gmail.com>
2023-04-09 18:30:45 -0400

    setup

--- scripts/mac_setup.sh
+++ scripts/mac_setup.sh
@@ -1,10 +1,26 @@
 echo "Start - mac_setup.sh"
 
+echo -e "\nUpdate Brew packages"
+brew update
+
 # Node
+echo -e "\nUpgrade Node by visiting the Node website"
+node -v
+
+echo -e "\nUpgrading NPM Packages"
+sudo npm -g outdated
+sudo npm -g upgrade --latest
+
+# Rust
+echo -e "\nUpgrading Rust"
+rustup update
+rustup -v
 
 # Nvim 
+echo -e "\nUpgrading NVIM"
+brew upgrade nvim
+
 # Alacritty
 
-echo "End - mac_setup.sh success"
-echo "End - mac_setup.sh failure"
+echo -e "\nEnd - mac_setup.sh"
 
