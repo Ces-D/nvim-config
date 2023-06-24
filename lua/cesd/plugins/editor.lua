694d1dc767f5d8a3407a7a9a7cb35313dd4c46e3
Ces-D <cesdia883@gmail.com>
2023-06-24 15:50:12 -0400

    theme

--- lua/cesd/plugins/editor.lua
+++ lua/cesd/plugins/editor.lua
@@ -89,56 +89,16 @@ return {
   },
 
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "lunarvim/Onedarker.nvim",
+    name = "onedarker",
     lazy = false,
     priority = 1000,
     config = function()
-      local theme = require("catppuccin")
+      local theme = require("onedarker")
 
-      theme.setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = {
-          light = "latte",
-          dark = "mocha",
-        },
-        transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-        term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false,              -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15,            -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false,              -- Force no italic
-        no_bold = false,                -- Force no bold
-        no_underline = false,           -- Force no underline
-        styles = {
-          comments = { "italic" },      -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          telescope = true,
-          mini = true,
-        },
-      })
+      theme.setup({})
 
-      vim.cmd.colorscheme "catppuccin"
+      vim.cmd.colorscheme "onedarker"
 
       -- local hr = tonumber(os.date('%H'))
 
