f7ec900c3787aa91978726a802697b56483ce601
Ces-D <58753718+Ces-D@users.noreply.github.com>
2024-07-23 17:30:38 -0400

    more themes

--- lua/me/plugins/editor.lua
+++ lua/me/plugins/editor.lua
@@ -72,61 +72,31 @@ return {
         end,
     },
 
-    -- {
-    --     "aktersnurra/no-clown-fiesta.nvim",
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         vim.opt.background = "light"
-    --         if vim.o.background == "dark" then
-    --             vim.cmd.colorscheme("no-clown-fiesta")
-    --         else
-    --             vim.cmd.colorscheme("default")
-    --         end
-    --
-    --         vim.api.nvim_create_autocmd({ "OptionSet" }, {
-    --             pattern = { "background" },
-    --             callback = function(ev)
-    --                 if vim.o.background == "dark" then
-    --                     -- vim.cmd.colorscheme("lunaperche") -- builtin theme
-    --                     vim.cmd.colorscheme("default") -- builtin theme
-    --                     -- vim.cmd.colorscheme("no-clown-fiesta")
-    --                 else
-    --                     -- vim.cmd.colorscheme("default") -- builtin theme
-    --                     vim.cmd.colorscheme("zellner") -- builtin theme
-    --                 end
-    --                 -- force a full redraw:
-    --                 vim.cmd("mode")
-    --             end,
-    --         })
-    --     end,
-    -- },
-
     {
-        "miikanissi/modus-themes.nvim",
-        priority = 1000,
+        "oahlen/iceberg.nvim",
         lazy = false,
         config = function()
             vim.opt.background = "light"
-            -- Default options
-            require("modus-themes").setup({
-                -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-                -- `auto` will automatically set style based on background set with vim.o.background
-                style = "auto",
-                variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-                transparent = false, -- Transparent background (as supported by the terminal)
-                dim_inactive = false, -- "non-current" windows are dimmed
-                hide_inactive_statusline = false, -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = true },
-                    keywords = { italic = true },
-                    functions = {},
-                    variables = {},
-                },
+            local dark_theme = "iceberg"
+            local light_theme = "iceberg-light"
+
+            if vim.o.background == "dark" then
+                vim.cmd.colorscheme(dark_theme)
+            else
+                vim.cmd.colorscheme(light_theme)
+            end
+
+            vim.api.nvim_create_autocmd({ "OptionSet" }, {
+                pattern = { "background" },
+                callback = function(_)
+                    if vim.o.background == "dark" then
+                        vim.cmd.colorscheme(dark_theme)
+                    else
+                        vim.cmd.colorscheme(light_theme) -- builtin theme
+                    end
+                    vim.cmd("mode") -- force a full redraw:
+                end,
             })
-            vim.cmd.colorscheme("modus") -- modus_operandi, modus_vivendi
         end,
     },
 }
